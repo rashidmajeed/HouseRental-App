aac930938bb6e6ef1486fee98476cfd9fa937af3
rashidmajeed <rashid.caliber@gmail.com>
2018-09-16 04:01:18 +0200

    update styling with the cards and apply navigation to logo onclick towards rentallist component

--- src/components/RentalCard.js
+++ src/components/RentalCard.js
@@ -6,11 +6,11 @@ const RentalCard = (props) => {
   const rental = props.rental;
   return (
     <div className='col-md-3 col-xs-6'>
-      <Link to={`/rentals/${rental.id}`}>
+      <Link className='rental-detail-link' to={`/rentals/${rental.id}`}>
         <div className='card houserental-card'>
-          <img className='card-img-top' src={rental.image} alt=''></img>
+          <img className='card-img-top' src={rental.image} alt='rental'></img>
           <div className='card-block'>
-            <h6 className='card-subtitle'>{rental.shared ? 'shared' : 'whole'}{rental.category} &#183; {rental.city}</h6>
+            <h6 className={`card-subtitle ${rental.category}`}>{rental.shared ? 'shared' : 'whole'}{rental.category} &#183; {rental.city}</h6>
             <h4 className='card-title'>{rental.title}</h4>
             <p className='card-text'>${rental.dailyRate} per Night &#183; Free Cancelation</p>
           </div>
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
         <nav className='navbar navbar-dark navbar-expand-lg'>
       <div className='container'>
-        <a className='navbar-brand' href=''>House Rental</a>
+      <Link className='navbar-brand' to={`/rentals`}>House Rental</Link>
         <form className='form-inline my-2 my-lg-0'>
           <input className='form-control mr-sm-2 bwm-search' type='search' placeholder="Try 'New York'" aria-label='Search'></input>
         <button className='btn btn-outline-success my-2 my-sm-0 btn-bwm-search' type='submit'>Search</button>
